906fb709ac1d730426e2849789af7fa3a0377acb
Aleksey Maksimov <makaleks@live.ru>
2017-07-12 02:40:51 +0300

    Static files location fix.

--- src/drec_stud_site/settings.py
+++ src/drec_stud_site/settings.py
@@ -125,5 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '../collectedstatic'
-STATICFILES_DIRS = ('static',)
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
