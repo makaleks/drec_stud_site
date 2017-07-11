5d3cce63ef64c5b2f776668c1bf1ba7aa7819460
Aleksey Maksimov <makaleks@live.ru>
2017-07-12 02:20:56 +0300

    Static collected.

--- src/drec_stud_site/settings.py
+++ src/drec_stud_site/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '../../collectedstatic'
-STATICFILES_DIRS = ('../static',)
+STATIC_ROOT = '../collectedstatic'
+STATICFILES_DIRS = ('static',)
