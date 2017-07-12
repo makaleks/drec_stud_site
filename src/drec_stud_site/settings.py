7bcc479c1382cf21be6079198a1d7f28e9422643
Aleksey Maksimov <makaleks@live.ru>
2017-07-12 14:00:35 +0300

    Static files fixes.

--- src/drec_stud_site/settings.py
+++ src/drec_stud_site/settings.py
@@ -18,6 +18,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'collectedstatic')
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
 )
