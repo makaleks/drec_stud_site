d69f5718285f47f15133eb12ebcdf8b49493fe4e
Aleksey Maksimov <makaleks@live.ru>
2017-07-12 14:12:08 +0300

    Renamed to more readable collected_static.

--- src/drec_stud_site/settings.py
+++ src/drec_stud_site/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'collectedstatic')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'collected_static')
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
 )
