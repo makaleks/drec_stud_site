e4cf0aaa2c953ba80d847d1435172584d70ce642
Aleksey Maksimov <makaleks@live.ru>
2017-08-03 15:20:57 +0300

    Preview field added.

--- src/news/models.py
+++ src/news/models.py
@@ -6,6 +6,8 @@ class News(models.Model):
     created = models.DateField(auto_now_add = True, verbose_name = 'Дата создания')
     edited = models.DateTimeField(auto_now = True, verbose_name = 'Последнее редактирование')
     title = models.CharField(max_length = 64, blank = False, null = False, verbose_name = 'Заголовок')
+    # Preview will be shown before spoiler
+    text_preview = models.TextField(blank = True, verbose_name = 'Превью')
     text = models.TextField(blank = True, verbose_name = 'Текст')
     image = models.ImageField(blank = True)
     class Meta:
