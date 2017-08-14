627920c37e99ab8b0ba9bcd95454983f3126a446
pda11111 <pda11111@ya.ru>
2017-08-14 23:35:06 +0300

    Design updates: added modal, modified news cutting, some minor bugfixes

--- swdc/preview.sh
+++ swdc/preview.sh
@@ -1,3 +1,6 @@
-sed -e '1!N;/<!--{% block content %}\n *{% endblock %}-->/{ r core/index.html' -e 'd}' core/base.html > preview/index.html
-sed -i 's/{{ STATIC_URL }}/..\/static\//' preview/index.html
+sed -e '/<!--{% block content %}-->/{ r core/index.html' -e 'd}' core/base.html > preview/index.html
+sed -i 's/{{ STATIC_URL }}/..\/static/' preview/index.html
 sed -i 's/{%.*%}//' preview/index.html
+sed -e '/<!--{% block content %}-->/{ r core/services/washing.html' -e 'd}' core/base.html > preview/washing.html
+sed -i 's/{{ STATIC_URL }}/..\/static/' preview/washing.html
+sed -i 's/{%.*%}//' preview/washing.html
