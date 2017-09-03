2be8e0d2be0cf5b452c98ae90b0dd5109c7473b6
pda11111 <pda11111@ya.ru>
2017-09-03 23:37:06 +0300

    Washing, services, alone survey pages added, css restructurize

--- swdc/preview.sh
+++ swdc/preview.sh
@@ -1,6 +1,15 @@
 sed -e '/<!--{% block content %}-->/{ r core/index.html' -e 'd}' core/base.html > preview/index.html
 sed -i 's/{{ STATIC_URL }}/..\/static/' preview/index.html
 sed -i 's/{%.*%}//' preview/index.html
+sed -e '/<!--{% block content %}-->/{ r core/services/index.html' -e 'd}' core/base.html > preview/services.html
+sed -i 's/{{ STATIC_URL }}/..\/static/' preview/services.html
+sed -i 's/{%.*%}//' preview/services.html
 sed -e '/<!--{% block content %}-->/{ r core/services/washing.html' -e 'd}' core/base.html > preview/washing.html
 sed -i 's/{{ STATIC_URL }}/..\/static/' preview/washing.html
 sed -i 's/{%.*%}//' preview/washing.html
+sed -e '/<!--{% block content %}-->/{ r core/survey.html' -e 'd}' core/base.html > preview/survey.html
+sed -i 's/{{ STATIC_URL }}/..\/static/' preview/survey.html
+sed -i 's/{%.*%}//' preview/survey.html
+cd static
+cat main.css index.css surveys.css services.css washing.css > content.css
+cd ../
