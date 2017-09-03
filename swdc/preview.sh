0d92c483f56852fdebaffeda5cac3da6a6f4577d
pda11111 <pda11111@ya.ru>
2017-09-04 00:12:15 +0300

    Surveys list rewrite, little bugfix

--- swdc/preview.sh
+++ swdc/preview.sh
@@ -10,6 +10,9 @@ sed -i 's/{%.*%}//' preview/washing.html
 sed -e '/<!--{% block content %}-->/{ r core/survey.html' -e 'd}' core/base.html > preview/survey.html
 sed -i 's/{{ STATIC_URL }}/..\/static/' preview/survey.html
 sed -i 's/{%.*%}//' preview/survey.html
+sed -e '/<!--{% block content %}-->/{ r core/surveys_list.html' -e 'd}' core/base.html > preview/survey_list.html
+sed -i 's/{{ STATIC_URL }}/..\/static/' preview/survey_list.html
+sed -i 's/{%.*%}//' preview/survey_list.html
 cd static
 cat main.css index.css surveys.css services.css washing.css > content.css
 cd ../
