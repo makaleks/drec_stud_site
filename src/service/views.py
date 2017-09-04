8ac03cfeb47c9f5b2dc00754f6016f10eb97547d
Aleksey Maksimov <makaleks@live.ru>
2017-09-04 09:47:16 +0300

    JSON format fixed.

--- src/service/views.py
+++ src/service/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
 import datetime
+import json
+import re
 from .models import Order
 
 # Create your views here.
@@ -16,8 +18,10 @@ def unlock(request, service_name):
     else:
         return HttpResponse('no')
 
+def to_H_M(t):
+    return re.sub(r'(?P<part>^|:)0', '\g<part>', t.strftime('%H:%M'))
 
 def list_update(request, service_name):
     today = datetime.datetime.now()
     orders = Order.objects.all().filter(Q(date_start = today.date()) & (Q(time_end__gt = today.time()) | Q(time_end = datetime.time(0,0,0))) | Q(date_start__gt = today.date())).order_by('date_start', 'time_end')
-    return HttpResponse(str([{'uid': o.user.uid, 'date_start': o.date_start, 'time_start': o.time_start, 'time_end': o.time_end} for o in orders]))
+    return HttpResponse(json.dumps([{'uid': o.user.uid, 'date_start': str(o.date_start), 'time_start': to_H_M(o.time_start), 'time_end': to_H_M(o.time_end)} for o in orders]))
