9dcf4e581d97d8394e263e05f40342905852802d
Aleksey Maksimov <makaleks@live.ru>
2017-09-16 07:59:57 +0300

    Bug of lock system field names fixed.

--- src/service/views.py
+++ src/service/views.py
@@ -10,10 +10,10 @@ from .models import Order, Service
 
 # Create your views here.
 
-def unlock(request, service_name):
+def unlock(request, slug):
     uid = request.GET.get('uid')
     today = datetime.datetime.now()
-    orders = Order.objects.all().filter(Q(user__uid = uid, item__service__url_id = service_name, date_start = today.date(), time_start__lte = today.time()) & (Q(time_end__gt = today.time()) | Q(time_end = datetime.time(0,0,0))))
+    orders = Order.objects.all().filter(Q(user__uid = uid, item__service__slug = slug, date_start = today.date(), time_start__lte = today.time()) & (Q(time_end__gt = today.time()) | Q(time_end = datetime.time(0,0,0))))
     #return HttpResponse(str([vars(o) for o in orders]))
     if orders:
         return HttpResponse('yes')
@@ -23,9 +23,9 @@ def unlock(request, service_name):
 def to_H_M(t):
     return re.sub(r'(?P<part>^|:)0', '\g<part>', t.strftime('%H:%M'))
 
-def list_update(request, service_name):
+def list_update(request, slug):
     today = datetime.datetime.now()
-    orders = Order.objects.all().filter(Q(date_start = today.date(), item__service__url_id = service_name) & (Q(time_end__gt = today.time()) | Q(time_end = datetime.time(0,0,0))) | Q(date_start__gt = today.date())).order_by('date_start', 'time_end')
+    orders = Order.objects.all().filter(Q(date_start = today.date(), item__service__slug = slug) & (Q(time_end__gt = today.time()) | Q(time_end = datetime.time(0,0,0))) | Q(date_start__gt = today.date())).order_by('date_start', 'time_end')
     return HttpResponse(json.dumps([{'uid': o.user.uid, 'date_start': str(o.date_start), 'time_start': to_H_M(o.time_start), 'time_end': to_H_M(o.time_end)} for o in orders]))
 
 class ServiceListView(ListView):
