0f1c972f52d3954785f2c1ee84a3600adbce7e5d
Aleksey Maksimov <makaleks@live.ru>
2017-09-30 10:11:20 +0300

    BBCodes added.

--- src/drec_stud_site/settings.py
+++ src/drec_stud_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'markdown_deux',
     'social_django',
     'reversion',
+    'precise_bbcode',
     # disable to save media on delete/update
     'django_cleanup',
     'adminsortable2',
