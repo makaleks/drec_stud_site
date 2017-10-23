b4f39479545ae72289e9d03a427c2b1d300abc24
pda11111 <pda11111@ya.ru>
2017-10-24 01:26:49 +0300

    Add features on washing, start other services draft

--- swdc/preview.sh
+++ swdc/preview.sh
@@ -11,9 +11,9 @@ for FILE in index survey surveys_list note chart; do
                 i=0;
                 while ( getline x < ("core/"FILE".html") > 0 )
                 {
-                    if(x ~ /{% endblock %}/){i=2;}
+                    if(x ~ /{% endblock %}/) { i=2;}
                     if(i == 1){print x;}
-                    if(x ~ /{% block head_extras %}/){i=1;}
+                    if(x ~ /{% block head_extras %}/) { i=1;}
                 };
                 close("core/"FILE".html");
             }
@@ -22,9 +22,9 @@ for FILE in index survey surveys_list note chart; do
                 i=0;
                 while ( getline x < ("core/"FILE".html") > 0 )
                 {
-                    if(x ~ /{% endblock %}/){i=2;}
-                    if(i == 1){print x;}
-                    if(x ~ /{% block content %}/){i=1;}
+                    if(x ~ /{% endblock %}/) { i=2;}
+                    if(i == 1) { print x;}
+                    if(x ~ /{% block content %}/) { i=1;}
                 };
                 close("core/"FILE".html");
             }
@@ -33,9 +33,9 @@ for FILE in index survey surveys_list note chart; do
                 i=0;
                 while ( getline x < ("core/"FILE".html") > 0 )
                 {
-                    if(x ~ /{% endblock %}/){i=2;}
-                    if(i == 1){print x;}
-                    if(x ~ /{% block body_extras %}/){i=1;}
+                    if(x ~ /{% endblock %}/) { i=2;}
+                    if(i == 1) { print x;}
+                    if(x ~ /{% block body_extras %}/) { i=1;}
                 };
                 close("core/"FILE".html");
             }
@@ -52,41 +52,42 @@ for FILE in index washing; do
     {
         if($0 ~ /{% [ _a-z]* %}/)
         {
-            if(/{% block head_extras %}/)
+            if($0 ~ /{% block head_extras %}/)
             {
                 i=0;
                 while ( getline x < ("core/services/"FILE".html") > 0 )
                 {
-                    if(x ~ /{% endblock %}/){i=2;}
-                    if(i == 1){print x;}
-                    if(x ~ /{% block head_extras %}/){i=1;}
+                    if(x ~ /{% endblock %}/) { i=2;}
+                    if(i == 1) { print x;}
+                    if(x ~ /{% block head_extras %}/) { i=1;}
                 };
                 close("core/services/"FILE".html");
-            };
-            if(/{% block content %}/)
+            }
+            else if($0 ~ /{% block content %}/)
             {
                 i=0;
                 while ( getline x < ("core/services/"FILE".html") > 0 )
                 {
-                    if(x ~ /{% endblock %}/){i=2;}
-                    if(i == 1){print x;}
-                    if(x ~ /{% block content %}/){i=1;}
+                    if(x ~ /{% endblock %}/) { i=2;}
+                    if(i == 1) { print x;}
+                    if(x ~ /{% block content %}/) { i=1;}
                 };
                 close("core/services/"FILE".html");
-            };
-            if(/{% block body_extras %}/)
+            }
+            else if($0 ~ /{% block body_extras %}/)
             {
                 i=0;
                 while ( getline x < ("core/services/"FILE".html") > 0 )
                 {
-                    if(x ~ /{% endblock %}/){i=2;}
-                    if(i == 1){print x;}
-                    if(x ~ /{% block body_extras %}/){i=1;}
+                    if(x ~ /{% endblock %}/) { i=2;}
+                    if(i == 1) { print x;}
+                    if(x ~ /{% block body_extras %}/) { i=1;}
                 };
                 close("core/services/"FILE".html");
-            };
+            }
+            else if(!($0 ~ /{% endblock %}/)) {print $0;}
         }
-        else {print $0;}
+        else { print $0;}
     }' core/base.html > preview/services/$FILE.html;
     sed -i "s/{{ STATIC_URL }}/..\/..\/static\//" preview/services/$FILE.html;
     sed -i "s/{[\{\%] [ _\.a-z]* [\%\}]}//g" preview/services/$FILE.html;
