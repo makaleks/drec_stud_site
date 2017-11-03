e74ea61e06eb1d141e0ce2fecc246edb0edf8b19
Aleksey Maksimov <makaleks@live.ru>
2017-11-03 13:40:54 +0300

    Service POST logs added.

--- src/service/urls.py
+++ src/service/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from .views import unlock, list_update, ServiceListView, ServiceDetailView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^(?P<slug>.+)/list-update/$', list_update),
     url(r'^(?P<slug>.+)/unlock/$', unlock),
     url(r'^(?P<slug>.+)/$', ServiceDetailView.as_view(), name = 'service'),
-    url(r'^$', ServiceListView.as_view()),
+    url(r'^$', csrf_exempt(ServiceListView.as_view())),
 ]
