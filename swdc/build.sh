1c946b54dfc3f036f86a1775a32dad9e3720b091
pda11111 <pda11111@ya.ru>
2017-11-30 19:29:45 +0300

    Q&A added, some bugfixes

--- swdc/build.sh
+++ swdc/build.sh
@@ -1,3 +1,3 @@
 cd static
-cat base.css main.css material.css surveys.css services.css washing.css note.css index.css > content.css
+cat base.css main.css material.css surveys.css services.css washing.css qanda.css note.css index.css  > content.css
 cd ../
