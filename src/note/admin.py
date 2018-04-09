bc3ba12cd087177a1d0ccb4e09eaf7ace1e51d8d
Aleksey Maksimov <makaleks@live.ru>
2018-04-09 13:12:33 +0300

    Bug on not accessed questions fixed.

--- src/note/admin.py
+++ src/note/admin.py
@@ -19,10 +19,10 @@ class NoteAdmin(CustomBaseAdmin, SortableAdminMixin, VersionAdmin):
         else:
             fields = [{f.name: str(getattr(obj, f.name))} for f in obj._meta.fields]
             logger.info('{0} \'{1}\' was created as {2}'.format(self.model.__name__, str(obj), str(fields)), extra={'user': request.user.get_full_name()})
-        super(NoteAdmin, self).save_model(request, obj, form, change)
+        return super(NoteAdmin, self).save_model(request, obj, form, change)
     def delete_model(self, request, obj):
         logger.info('{0} \'{1}\' was deleted'.format(self.model.__name__, str(obj)), extra={'user': request.user.get_full_name()})
-        super(NoteAdmin, self).delete_model(request, obj)
+        return super(NoteAdmin, self).delete_model(request, obj)
 admin.site.register(Note, NoteAdmin)
 
 
@@ -40,8 +40,15 @@ class QuestionAdmin(CustomBaseAdmin, VersionAdmin):
         else:
             fields = [{f.name: str(getattr(obj, f.name))} for f in obj._meta.fields]
             logger.info('{0} \'{1}\' was created as {2}'.format(self.model.__name__, str(obj), str(fields)), extra={'user': request.user.get_full_name()})
-        super(QuestionAdmin, self).save_model(request, obj, form, change)
+        return super(QuestionAdmin, self).save_model(request, obj, form, change)
     def delete_model(self, request, obj):
         logger.info('{0} \'{1}\' was deleted'.format(self.model.title, str(obj)), extra={'user': request.user.get_full_name()})
-        super(QuestionAdmin, self).delete_model(request, obj)
+        return super(QuestionAdmin, self).delete_model(request, obj)
+    # Questions are accessable for every is_staff
+    def has_add_permission(self, request):
+        return True
+    def has_change_permission(self, request, obj=None):
+        return True
+    def has_delete_permission(self, request, obj=None):
+        return True
 admin.site.register(Question, QuestionAdmin)
