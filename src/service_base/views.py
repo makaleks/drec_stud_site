e83b3bea64b5b586f15a595fdb9780f95634ff03
Aleksey Maksimov <makaleks@live.ru>
2018-10-02 20:11:39 +0300

    Bug on Service update fixed #12.

--- src/service_base/views.py
+++ src/service_base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
 from django.apps import apps
 
