c4a5c80c00168db893352c35492223d00ef704fa
pda11111 <pda11111@ya.ru>
2018-11-06 02:12:54 +0300

    half rewrited to haml, tabs logic rewrited, sass fully rewrited

--- swdc/gen.py
+++ swdc/gen.py
@@ -9,7 +9,38 @@ gento = "../gen/"
 haml = ["core/base",
         "core/error",
         "core/login",
-        "reversion/revision_form"
+        "comment/admin/comment/comment/change_form",
+        "meeting_room/admin/meeting_room/meetingroom/change_form",
+        "meeting_room/meeting_room_timetable",
+#        "news/admin/news/news/change_form",
+#        "news/archive_select",
+#        "news/news_list",
+        "note/admin/note/question/change_form",
+        "note/admin/note/note/change_form",
+        "note/note_detail",
+        "note/note_list",
+        "note/qanda",
+#        "note/student_council",
+        "reversion/revision_form",
+        "service/admin/service/service/change_form"#,
+#        "service_base/service_base",
+#        "service_base/service_list",
+#        "service_document/service_document_timetable",
+#        "service_item/service_item_timetable",
+#        "service/service_base1",
+#        "service/service_list1",
+#        "service/service_timetable",
+#        "service/service_timetable_single",
+#        "survey/admin/survey/survey/change_form",
+#        "survey/chart",
+#        "survey/survey_detail",
+#        "survey/survey_list",
+#        "survey/survey_thanks",
+#        "user/admin/user/user/change_form",
+#        "user/long_logout",
+#        "user/uid_set",
+#        "washing/admin/washing/washing/change_form",
+#        "washing/washing_timetable"
         ]
 
 def pregen():
@@ -18,6 +49,16 @@ def pregen():
         os.makedirs(compiled_folder)
     if not os.path.exists(compiled_folder + "/core"):
         os.makedirs(compiled_folder + "/core")
+    if not os.path.exists(compiled_folder + "/comment/admin/comment/comment"):
+        os.makedirs(compiled_folder + "/comment/admin/comment/comment")
+    if not os.path.exists(compiled_folder + "/meeting_room/admin/meeting_room/meetingroom"):
+        os.makedirs(compiled_folder + "/meeting_room/admin/meeting_room/meetingroom")
+    if not os.path.exists(compiled_folder + "/note/admin/note/question"):
+        os.makedirs(compiled_folder + "/note/admin/note/question")
+    if not os.path.exists(compiled_folder + "/note/admin/note/note"):
+        os.makedirs(compiled_folder + "/note/admin/note/note")
+    if not os.path.exists(compiled_folder + "/service/admin/service/service"):
+        os.makedirs(compiled_folder + "/service/admin/service/service")
     if not os.path.exists(compiled_folder + "/reversion"):
         os.makedirs(compiled_folder + "/reversion")
 
