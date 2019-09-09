5287d4dca3a185cc7539c2ef0f861c1770479590
Aleksey Maksimov <makaleks@live.ru>
2019-09-09 21:35:46 +0300

    Bug on show_user_info() fixed.

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -22,27 +22,38 @@
     }
 })();
 function showuserinfo(name, vkid, number, email) {
-    var username = document.querySelector('#userinfo_name'),
-        uservkid = document.querySelector('#userinfo_vkid'),
-        usernumber = document.querySelector('#userinfo_number'),
-        useremail = document.querySelector('#userinfo_email');
-    usernumber.classList.add('hidden');
-    uservkid.classList.add('hidden');
-    useremail.classList.add('hidden');
-    username.innerText = name;
-    if (number !== null) {
-        usernumber.innerText = 'Позвонить: ' + number;
-        usernumber.href = 'tel:' + number;
-        usernumber.classList.remove('hidden');
+    var content = document.querySelector('#modal_userinfo_content');
+    while (content.firstChild)
+        content.removeChild(content.firstChild);
+    var details_added = false;
+    if (name) {
+        var el_name = document.createElement('h3');
+        el_name.innerText = name;
+        content.appendChild(el_name);
     }
-    if (vkid !== null) {
-        uservkid.innerText = 'Написать в ВК vk.com/id' + vkid;
-        uservkid.href = 'https://vk.me/id' + vkid;
-        uservkid.classList.remove('hidden');
+    if (number) {
+        var el_number = document.createElement('a');
+        el_number.innerText = 'Позвонить: ' + number;
+        el_number.href = 'tel:' + number;
+        content.appendChild(el_number);
+        details_added = true;
     }
-    if (email !== null) {
-        useremail.innerText = 'Написать: ' + email;
-        useremail.href = 'mailto:' + email;
-        useremail.classList.remove('hidden');
+    if (vkid) {
+        if (details_added)
+            content.appendChild(document.createElement('br'));
+        var el_vkid = document.createElement('a');
+        el_vkid.innerText = 'Написать в ВК vk.com/id' + vkid;
+        el_vkid.href = 'https://vk.me/id' + vkid;
+        content.appendChild(el_vkid);
+        details_added = true;
+    }
+    if (email) {
+        if (details_added)
+            content.appendChild(document.createElement('br'));
+        var el_email = document.createElement('a');
+        el_email.innerText = 'Написать: ' + email;
+        el_email.href = 'mailto:' + email;
+        content.appendChild(el_email);
+        details_added = true;
     }
 };
