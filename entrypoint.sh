c037c66b5eaf4f48e2938396f2b0a96f678f58aa
alekseik1 <1alekseik1@gmail.com>
2021-05-12 00:22:02 +0300

    fix(docker): path to pgdump_helper script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ chmod +x manage.py
 ./manage.py collectstatic
 cd ..
 chmod +x pgdump_helper.py
-# ./pgdump_helper.py --restore -f "db_backup.sql"
+#./scripts/pgdump_helper.py --restore -f "db_backup.sql"
 
 
 gunicorn -b 0.0.0.0:8080 -w 4 --pythonpath /app/src drec_stud_site.wsgi:application
