a42198887257a78dd1ed8556b0b19834b5f0d4af
alekseik1 <1alekseik1@gmail.com>
2021-06-06 14:36:13 +0300

    test(bot): cleanup commands cases

--- vk_bot/tests/vk_handling/test_handlers_cases.py
+++ vk_bot/tests/vk_handling/test_handlers_cases.py
@@ -1,5 +1,5 @@
 from pytest_cases import case, parametrize
-from src.commands import AdminOpenLock5, AdminOpenLock6, AdminCloseLock5, AdminCloseLock6
+from src.commands import AdminOpenLock5, AdminOpenLock6, AdminCloseLock5, AdminCloseLock6, HelpCommand, GetPasswordCommand
 from src.blueprints.admin import handle_open_5b, handle_open_6b, handle_close_5b, handle_close_6b
 from src.blueprints.defaults import hello_admin
 from src.blueprints.help import print_help
@@ -8,35 +8,33 @@ from src.settings import ADMIN_HARDCODED_LIST
 
 
 @case(tags='from_admin')
-@parametrize(**{'from_id': ADMIN_HARDCODED_LIST})
-class DoorControlCases:
-    def case_open_5b(self, from_id):
+@parametrize(**{'from_id': ADMIN_HARDCODED_LIST, 'command_prefix': ['!', '/']})
+class AdminCases:
+    def case_open_5b(self, from_id, command_prefix):
         # text, id, handler_function
-        return '!' + AdminOpenLock5.raw_message_name, from_id, handle_open_5b
+        return command_prefix + AdminOpenLock5.raw_message_name, from_id, handle_open_5b
 
-    def case_open_6b(self, from_id):
+    def case_open_6b(self, from_id, command_prefix):
         # text, id, handler_function
-        return '!' + AdminOpenLock6.raw_message_name, from_id, handle_open_6b
+        return command_prefix + AdminOpenLock6.raw_message_name, from_id, handle_open_6b
 
-    def case_close_5b(self, from_id):
+    def case_close_5b(self, from_id, command_prefix):
         # text, id, handler_function
-        return '!' + AdminCloseLock5.raw_message_name, from_id, handle_close_5b
+        return command_prefix + AdminCloseLock5.raw_message_name, from_id, handle_close_5b
 
-    def case_close_6b(self, from_id):
+    def case_close_6b(self, from_id, command_prefix):
         # text, id, handler_function
-        return '!' + AdminCloseLock6.raw_message_name, from_id, handle_close_6b
+        return command_prefix + AdminCloseLock6.raw_message_name, from_id, handle_close_6b
 
+    @parametrize(**{'text': ['command fialw', 'фывйцв', 'smtgsh']})
+    def case_unknown_message(self, text, from_id, command_prefix):
+        return text, from_id, hello_admin
 
-@parametrize(**{'text': ['command fialw', 'фывйцв', 'smtgsh'], 'from_id': ADMIN_HARDCODED_LIST})
-@case(tags='from_admin')
-def case_unknown_message(text, from_id):
-    return text, from_id, hello_admin
+    def case_help_message(self, from_id, command_prefix):
+        return command_prefix + HelpCommand.raw_message_name, from_id, print_help
 
-
-@parametrize(**{'text': ['!помощь', '/помощь'], 'from_id': ADMIN_HARDCODED_LIST})
-@case(tags='from_admin')
-def case_help_message(text, from_id):
-    return text, from_id, print_help
+    def case_password_message(self, from_id, command_prefix):
+        return command_prefix + GetPasswordCommand.raw_message_name, from_id, send_emergency_password
 
 
 # Проверка на то, что на запретной команде сваливается в `hello_admin`
