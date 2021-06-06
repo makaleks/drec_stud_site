6f1889736a358f4cda914f3b7fa82ea69349582d
alekseik1 <1alekseik1@gmail.com>
2021-06-06 14:18:41 +0300

    test(bot): parametrize id for admins

--- vk_bot/tests/vk_handling/test_handlers_cases.py
+++ vk_bot/tests/vk_handling/test_handlers_cases.py
@@ -4,44 +4,42 @@ from src.blueprints.admin import handle_open_5b, handle_open_6b, handle_close_5b
 from src.blueprints.defaults import hello_admin
 from src.blueprints.help import print_help
 from src.blueprints.emergency_password import send_emergency_password
-from src.settings import TECHNICAL_ADMIN_ID
+from src.settings import ADMIN_HARDCODED_LIST
 
 
 @case(tags='from_admin')
+@parametrize(**{'from_id': ADMIN_HARDCODED_LIST})
 class DoorControlCases:
-    def case_open_5b(self):
+    def case_open_5b(self, from_id):
         # text, id, handler_function
-        return '!' + AdminOpenLock5.raw_message_name, TECHNICAL_ADMIN_ID, handle_open_5b
+        return '!' + AdminOpenLock5.raw_message_name, from_id, handle_open_5b
 
-    def case_open_6b(self):
+    def case_open_6b(self, from_id):
         # text, id, handler_function
-        return '!' + AdminOpenLock6.raw_message_name, TECHNICAL_ADMIN_ID, handle_open_6b
+        return '!' + AdminOpenLock6.raw_message_name, from_id, handle_open_6b
 
-    def case_close_5b(self):
+    def case_close_5b(self, from_id):
         # text, id, handler_function
-        return '!' + AdminCloseLock5.raw_message_name, TECHNICAL_ADMIN_ID, handle_close_5b
+        return '!' + AdminCloseLock5.raw_message_name, from_id, handle_close_5b
 
-    def case_close_6b(self):
+    def case_close_6b(self, from_id):
         # text, id, handler_function
-        return '!' + AdminCloseLock6.raw_message_name, TECHNICAL_ADMIN_ID, handle_close_6b
+        return '!' + AdminCloseLock6.raw_message_name, from_id, handle_close_6b
 
 
-@parametrize('text', [
-    'command fialw', 'фывйцв', 'smtgsh'
-])
-def case_unknown_message(text):
-    return text, TECHNICAL_ADMIN_ID, hello_admin
+@parametrize(**{'text': ['command fialw', 'фывйцв', 'smtgsh'], 'from_id': ADMIN_HARDCODED_LIST})
+@case(tags='from_admin')
+def case_unknown_message(text, from_id):
+    return text, from_id, hello_admin
 
 
-@parametrize('text', [
-    '!помощь', '/помощь'
-])
-def case_help_message(text):
-    return text, TECHNICAL_ADMIN_ID, print_help
+@parametrize(**{'text': ['!помощь', '/помощь'], 'from_id': ADMIN_HARDCODED_LIST})
+@case(tags='from_admin')
+def case_help_message(text, from_id):
+    return text, from_id, print_help
 
 
-@parametrize('text', [
-    '!пароль', '/пароль'
-])
-def case_password_message(text):
-    return text, TECHNICAL_ADMIN_ID, send_emergency_password
+@parametrize(**{'text': ['!пароль', '/пароль'], 'from_id': ADMIN_HARDCODED_LIST})
+@case(tags='from_admin')
+def case_password_message(text, from_id):
+    return text, from_id, send_emergency_password
