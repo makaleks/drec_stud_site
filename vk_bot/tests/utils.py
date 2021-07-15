ec075bc4196693ca1c1bb159bf966de6236dbf23
alekseik1 <1alekseik1@gmail.com>
2021-07-15 22:30:09 +0300

    refactor(test): move fake API builder to separate file
    Makes it easier to reuse

---
+++ vk_bot/tests/utils.py
@@ -0,0 +1,92 @@
+import datetime
+import json
+from typing import List
+
+from pydantic import BaseModel
+from vkbottle import API
+from vkbottle.tools.test_utils import MockedClient
+
+from src.settings import GROUP_ID, TECHNICAL_ADMIN_ID
+
+
+class Message(BaseModel):
+    text: str
+    from_id: int = TECHNICAL_ADMIN_ID
+    on_datetime: datetime.datetime
+
+
+def build_raw_vk_message_event(message: Message) -> dict:
+    """Сгенерить одно сообщение ВК."""
+    return {
+        "type": "message_new",
+        "object": {
+            "message": {
+                "date": int(message.on_datetime.timestamp()),
+                "from_id": message.from_id,
+                "id": 1771,
+                "out": 0,
+                "peer_id": message.from_id,
+                "text": message.text,
+                "conversation_message_id": 1302,
+                "fwd_messages": [],
+                "important": False,
+                "random_id": 0,
+                "attachments": [],
+                "is_hidden": False,
+            },
+            "client_info": {
+                "button_actions": [
+                    "text",
+                    "intent_subscribe",
+                    "intent_unsubscribe",
+                ],
+                "keyboard": True,
+                "inline_keyboard": False,
+                "carousel": False,
+                "lang_id": 3,
+            },
+        },
+        "group_id": GROUP_ID,
+        "event_id": "15cd5841e6fa2ba10c4b4d114a8dc04f78fc47b4",
+    }
+
+
+def build_updates(list_messages: List[Message]):
+    """Сгенерить апдейт из кучи сообщений."""
+    return {
+        "ts": 1,
+        "updates": [build_raw_vk_message_event(message) for message in list_messages],
+    }
+
+
+def build_fake_api(list_messages: List[Message]):
+    # Все подобрано как отвечает реальный ВК, поэтому дважды подумайте, прежде чем менять
+    # Забрал с https://github.com/timoniq/vkbottle/blob/c6ebefb/tests/bot_test.py
+    def callback(data: dict):
+        # Этот парень нужен
+        if "groups.getById" in data["url"]:
+            return {"response": [{"id": 1}]}
+        # И этот тоже
+        elif "groups.getLongPollServer" in data["url"]:
+            return {"response": {"ts": 1, "server": "!SERVER!", "key": ""}}
+        # И этот тоже
+        elif "!SERVER!" in data["url"]:
+            return build_updates(list_messages=list_messages)
+        # Отправка сообщений со стороны бота.
+        elif "messages.send" in data["url"]:
+            target_data = data["data"]
+            if "keyboard" in target_data:
+                try:
+                    json.dumps(target_data["keyboard"])
+                except TypeError:
+                    target_data["keyboard"] = target_data["keyboard"].get_json()
+            return json.dumps({"response": {**data["data"], **{"r": 1}}})
+
+    # Создадим API, в котором клиент будет ходить на коллбеки выше ВМЕСТО vk.com
+    api = API("token")
+    api.http._session = MockedClient(None, callback=callback)
+    return api
+
+
+def build_fake_bot(list_messages: List[Message]):
+    pass
--- vk_bot/tests/vk_handling/cases_vk_messages.py
+++ vk_bot/tests/vk_handling/cases_vk_messages.py
@@ -1,15 +1,25 @@
 from pytest_cases import case, parametrize
-from src.commands import AdminOpenLock5, AdminOpenLock6, AdminCloseLock5, AdminCloseLock6, HelpCommand, GetPasswordCommand
+
 from src.blueprints.admin import handle_close_5b, handle_close_6b
-from src.blueprints.door import handle_open_5b, handle_open_6b
 from src.blueprints.defaults import hello_admin
-from src.blueprints.help import print_help
+from src.blueprints.door import handle_open_5b, handle_open_6b
 from src.blueprints.emergency_password import send_emergency_password
+from src.blueprints.help import print_help
+from src.blueprints.reporting import report_problem_start
+from src.commands import (
+    AdminCloseLock5,
+    AdminCloseLock6,
+    AdminOpenLock5,
+    AdminOpenLock6,
+    GetPasswordCommand,
+    HelpCommand,
+    ReportProblemStart,
+)
 from src.settings import ADMIN_HARDCODED_LIST
 
 
-@case(tags='from_admin')
-@parametrize(**{'from_id': ADMIN_HARDCODED_LIST, 'command_prefix': ['!', '/']})
+@case(tags="from_admin")
+@parametrize(**{"from_id": ADMIN_HARDCODED_LIST, "command_prefix": ["!", "/"]})
 class AdminCases:
     def case_open_5b(self, from_id, command_prefix):
         # text, id, handler_function
@@ -21,13 +31,21 @@ class AdminCases:
 
     def case_close_5b(self, from_id, command_prefix):
         # text, id, handler_function
-        return command_prefix + AdminCloseLock5.raw_message_name, from_id, handle_close_5b
+        return (
+            command_prefix + AdminCloseLock5.raw_message_name,
+            from_id,
+            handle_close_5b,
+        )
 
     def case_close_6b(self, from_id, command_prefix):
         # text, id, handler_function
-        return command_prefix + AdminCloseLock6.raw_message_name, from_id, handle_close_6b
+        return (
+            command_prefix + AdminCloseLock6.raw_message_name,
+            from_id,
+            handle_close_6b,
+        )
 
-    @parametrize(**{'text': ['command fialw', 'фывйцв', 'smtgsh']})
+    @parametrize(**{"text": ["command fialw", "фывйцв", "smtgsh"]})
     def case_unknown_message(self, text, from_id, command_prefix):
         return text, from_id, hello_admin
 
@@ -35,17 +53,30 @@ class AdminCases:
         return command_prefix + HelpCommand.raw_message_name, from_id, print_help
 
     def case_password_message(self, from_id, command_prefix):
-        return command_prefix + GetPasswordCommand.raw_message_name, from_id, send_emergency_password
+        return (
+            command_prefix + GetPasswordCommand.raw_message_name,
+            from_id,
+            send_emergency_password,
+        )
+
+    def case_report_problem(self, from_id, command_prefix):
+        return (
+            command_prefix + ReportProblemStart.raw_message_name,
+            from_id,
+            report_problem_start,
+        )
 
 
 # Проверка на то, что на запретной команде сваливается в `hello_admin`
-@case(tags='from_nonadmin')
-@parametrize(**{
-    'from_id': [188477847],
-    'command_prefix': ['!', '/'],
-    # Сюда пишите команды, которые запрещены обычным пользователям
-    'command': [AdminCloseLock5, AdminCloseLock6]
-})
+@case(tags="from_nonadmin")
+@parametrize(
+    **{
+        "from_id": [188477847],
+        "command_prefix": ["!", "/"],
+        # Сюда пишите команды, которые запрещены обычным пользователям
+        "command": [AdminCloseLock5, AdminCloseLock6],
+    }
+)
 class RestrictedCommandsCases:
     def case_forbidden_command(self, from_id, command_prefix, command):
         # text, id, handler_function
