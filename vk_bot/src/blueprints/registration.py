5196a4960db2fc56ff214cc287cd6bd5fae52451
alekseik1 <1alekseik1@gmail.com>
2021-08-23 23:10:28 +0300

    feat(vk-bot): registration final steps

--- vk_bot/src/blueprints/registration.py
+++ vk_bot/src/blueprints/registration.py
@@ -1,3 +1,6 @@
+import os
+
+import aiohttp
 from loguru import logger
 from vkbottle import BotBlueprint
 from vkbottle.bot import Message, rules
@@ -69,7 +72,9 @@ async def process_room_entry(message: Message, **kwargs):
             keyboard=build_backwards_keyboard(),
         )
     else:
-        await bl.state_dispenser.set(user_id, RegistrationStates.APPROVING_INPUT)
+        await bl.state_dispenser.set(
+            user_id, RegistrationStates.APPROVING_INPUT, room_number=room_number
+        )
         user = await message.get_user()
         name, surname, group, room_number = (
             user.first_name,
@@ -105,8 +110,44 @@ async def registration_finish(message: Message, **kwargs):
         )
     else:
         await bl.state_dispenser.set(user_id, RegistrationStates.DEFAULT)
-        await message.answer(
-            message="Зарегистрировал тебя в стиралку. "
-            'Теперь можно зайти в https://8ka.mipt.ru и нажать кнопку "Войти через ВК".',
-            keyboard=build_keyboard(is_admin=is_admin(user_id)),
+        # Get info
+        user = await message.get_user()
+        name, surname, group, room_number = (
+            user.first_name,
+            user.last_name,
+            message.state_peer.payload["group_number"],
+            message.state_peer.payload["room_number"],
         )
+        # Send registration request to the website
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f'{os.environ["STIRKA_SITE"]}/user/register',
+                json={
+                    "token": os.environ["STIRKA_REGISTRATION_TOKEN"],
+                    "account_id": user_id,
+                    "last_name": surname,
+                    "first_name": name,
+                    "room_number": room_number,
+                    "group_number": group,
+                },
+            ) as response:
+                error = None
+                try:
+                    resp_text = await response.text()
+                    print(resp_text)
+                    if response.status != 400:
+                        error = resp_text
+                except Exception as e:
+                    error = e
+                if error is not None:
+                    await message.answer(
+                        message=f"Что-то пошло не так, вот ошибка {error}\n\n"
+                        f"Попробуй переслать это сообщение @id{TECHNICAL_ADMIN_ID}",
+                        keyboard=build_keyboard(is_admin(user_id)),
+                    )
+                else:
+                    await message.answer(
+                        message="Зарегистрировал тебя в стиралку. "
+                        'Теперь можно зайти в https://8ka.mipt.ru и нажать кнопку "Войти через ВК".',
+                        keyboard=build_keyboard(is_admin=is_admin(user_id)),
+                    )
